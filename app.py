4f9e3a6a02bb5a1d382f20d8b6669ae9a486b634
SuchLuukie <62255292+SuchLuukie@users.noreply.github.com>
2020-11-18 14:54:44 +0100

    Progress

--- app.py
+++ app.py
@@ -1,4 +1,54 @@
 # Import files and libraries.
+from flask import Flask
+import pandas as pd
+import folium
+import json
+
 from update import UpdateData
 
+app = Flask(__name__)
 UpdateData()
+
+@app.route("/")
+def base():
+	map = update()
+	return map._repr_html_()
+		
+
+data = pd.read_csv("data.csv")
+def update():
+	geo = loadJSON("geojson.json")
+
+	map = folium.Map(
+		location=[-0.023559, 37.9061928],
+		max_bounds=True,
+		zoom_start=2,
+		)
+
+	folium.Choropleth(
+	    geo_data=geo,
+	    name='choropleth',
+	    data=data,
+	    columns=['country', 'cases'],
+	    key_on='feature.properties.name',
+	    fill_color='YlGn',
+	    fill_opacity=0.7,
+	    line_opacity=0.2,
+	    legend_name='Total Cases'
+	).add_to(map)
+
+	return map
+
+
+def loadJSON(filename):
+	with open(filename, "r") as json_file:
+		return json.load(json_file)
+
+
+def writeJSON(filename, data):
+	with open(filename, 'w') as f:
+		json.dump(data, f, indent=4)
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
--- update.py
+++ update.py
@@ -1,21 +1,30 @@
 import requests
 import json
+import pandas as pd
+import datetime
+import csv
+from csv import reader
 
 class UpdateData():
 	"""docstring for UpdateData"""
 	def __init__(self):
 		self.data_url = "https://covid.ourworldindata.org/data/owid-covid-data.json"
+		self.csv_url = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
 
 		self.update_data()
 		
 	
+	"""The main function that updates data.json and data.csv to the new dataset"""
 	def update_data(self):
-		data = requests.get(self.data_url).json()
-		data = self.assignGeocode(data)
+		#data = requests.get(self.data_url).json()
+		#data = self.assignGeocode(data)
 
-		self.writeJSON("data.json", data)
+		#self.writeJSON("data.json", data)
+
+		self.update_csv()
 
 
+	"""Assigns the middle of the country's coordinate to the dictionary."""
 	def assignGeocode(self, data):
 		for item in data:
 			geocode = self.findGeocode(data[item]["location"])
@@ -24,6 +33,7 @@ class UpdateData():
 		return data
 
 
+	"""Finds the geocode to the given country."""
 	def findGeocode(self, country):
 		print(country)
 		url = 'https://nominatim.openstreetmap.org/search/{}?format=json'.format(country)
@@ -31,11 +41,39 @@ class UpdateData():
 		return [response[0]["lat"], response[0]["lon"]]
 
 
+	"""Updates the CSV file to only display the current day's date"""
+	def update_csv(self):
+		data = self.loadJSON("data.json")
+
+		dictionary = {"country": {}, "cases": {}}
+		count = 0
+		for item in data:
+			country = data[item]["location"]
+
+
+			try:
+				cases = data[item]["data"][-1]["total_cases"]
+			except KeyError:
+				pass
+
+			if country != "World":
+				dictionary["country"].update({count: country})
+				dictionary["cases"].update({count: round(cases)})
+				count += 1
+
+		self.writeJSON("csv.json", dictionary)
+
+		df = pd.read_json("csv.json")
+		df.to_csv("data.csv", index=None)
+
+
+	"""Returns a json file."""
 	def loadJSON(self, filename):
 		with open(filename, "r") as json_file:
 			return json.load(json_file)
 
 
+	"""Writes the data to the file."""
 	def writeJSON(self, filename, data):
 		with open(filename, 'w') as f:
 			json.dump(data, f, indent=4)
